2081c9a61181f97cb2379500056408f4c6112c98
KhoriF <khorif11@yahoo.com>
2024-02-01 22:23:15 -0600

    Added use of events and forms

--- src/App.js
+++ src/App.js
@@ -1,67 +1,24 @@
 import "./App.css";
-import Info from "./Info.js";
-import {PropTypes} from "prop-types";
+import SearchBar from "./SearchBar";
 import {useState} from "react";
 
 function App() {
+const [data, setData] = useState({});
+
+const updateData = (searchParams) => {
+  setData(searchParams);
+}
+
   return (
     <div className="App">
-      <Info title="Test"></Info>
-      <ButtonState></ButtonState>
+      <SearchBar callback={updateData}></SearchBar>
+      <p>{"name" in data ? data["name"] : "No Data to display"}</p>
+      <p>{"price" in data ? data["price"] : "No Data to display"}</p>
+      <p>{"type" in data ? data["type"] : "No Data to display"}</p>
+      <p>{"brand" in data ? data["brand"] : "No Data to display"}</p>
     </div>
   );
 }
-
-// Component will be re-rendered from state
-function ButtonState() {
-
-  // Initialize state variables and functions to update the states
-  const [title, setTitle] = useState("");
-  const [count, setCount] = useState(0);
-
-  const updateTitleClicked = () => {
-    setTitle("New");
-  }
-
-  const updateCounterClicked = () => {
-      setCount(count + 1);
-  }
-
-  const resetCounter = () => {
-    setCount(0);
-  }
-
-  return (
-    <div>
-    {/* State used as a prop*/}
-    <Data title={title} count={count}></Data>
-    <button onClick={updateTitleClicked}>Update Title</button>
-    <button onClick={updateCounterClicked}>Update Counter</button>
-    <button onClick={resetCounter}>Reset Counter</button>
-    </div>
-  )
-}
-
-function Data(props) {
-  return (
-    <div>
-      <p>Title: {props.title}</p>
-      <p>Count: {props.count}</p>
-    </div>
-  );
-}
-
-
-// AddItem.defaultProps = {
-//   number: 7,
-//   text: "default",
-// };
-
-// // Prop types are used to enforce the the correct typing for props
-// AddItem.propTypes = {
-//   number: PropTypes.number,
-//   text: PropTypes.string
-// }
 
 
 
