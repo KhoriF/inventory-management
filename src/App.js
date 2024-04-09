ffd335b1b8f18bd66f5550e7643435f66f84f62b
KhoriF <khorif11@yahoo.com>
2024-04-08 20:57:05 -0500

    Added bootstrap utilization for styling

--- src/App.js
+++ src/App.js
@@ -2,35 +2,38 @@ import "./App.css";
 import SearchBar from "./SearchBar";
 import AddItem from "./AddItem";
 import ItemsDisplay from "./ItemsDisplay";
-import {useState} from "react";
-
+import { useState } from "react";
 
 function App() {
-const [filters, setFilters] = useState({});
-const [data, setData] = useState({items: []});
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
 
-const updateFilters = (searchParams) => {
-  setFilters(searchParams);
-};
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  };
 
-const addItemToData = (item) => {
-  // Storing state in currentData
-  let items = data["items"];
-  item.id = items.length;
-  items.push(item);
-  setData({items:items});
-  console.log(data);
-};
+  const addItemToData = (item) => {
+    // Storing state in currentData
+    let items = data["items"];
+    item.id = items.length;
+    items.push(item);
+    setData({ items: items });
+    console.log(data);
+  };
 
   return (
-    <div className="App">
-      <SearchBar updateSearchParams={updateFilters}></SearchBar>
-      <ItemsDisplay items={data["items"]}></ItemsDisplay>
-      <AddItem addItem={addItemToData}></AddItem>
+    <div className="container">
+      <div className="row mt-3">
+        <SearchBar updateSearchParams={updateFilters}></SearchBar>
+      </div>
+      <div className="row mt-3">
+        <ItemsDisplay items={data["items"]}></ItemsDisplay>
+      </div>
+      <div className="row mt-3">
+        <AddItem addItem={addItemToData}></AddItem>
+      </div>
     </div>
   );
 }
-
-
 
 export default App;
