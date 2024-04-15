08fc5e4a38c5a31473979129776b595da23def29
KhoriF <khorif11@yahoo.com>
2024-04-14 19:24:15 -0500

    Added JSON Server and fetch request

--- src/App.js
+++ src/App.js
@@ -15,10 +15,50 @@ function App() {
   const addItemToData = (item) => {
     // Storing state in currentData
     let items = data["items"];
-    item.id = items.length;
-    items.push(item);
-    setData({ items: items });
-    console.log(data);
+
+    const requestOptions = {
+      // Need to have header to send post request
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    };
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+      });
+  };
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name) {
+      return data;
+    }
+
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+
+      if (filters.price !== 0 && item.name > filters.price) {
+        continue;
+      }
+
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+
+      filteredData.push(item);
+    }
+    return filteredData;
   };
 
   return (
@@ -27,7 +67,7 @@ function App() {
         <SearchBar updateSearchParams={updateFilters}></SearchBar>
       </div>
       <div className="row mt-3">
-        <ItemsDisplay items={data["items"]}></ItemsDisplay>
+        <ItemsDisplay items={filterData(data["items"])}></ItemsDisplay>
       </div>
       <div className="row mt-3">
         <AddItem addItem={addItemToData}></AddItem>
